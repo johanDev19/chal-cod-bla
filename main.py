1bb853fed7fb2c84656fe347799571487d06324f
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-14 09:11:50 -0500

    build: add python-dontenv

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 app = Flask(__name__)
+load_dotenv()
 
 
 @app.route('/')
 def main():
     return 'c4v!'
+
+
+@app.route('/tweets')
+def list_tweets():
+    print(os.getenv('TWITTER_CONSUMER_KEY'))
+    return 'All ok!'
